0a5e5020bfd05711a76f1a4af4a8b5e3f129bfaf
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-15 09:38:34 -0700

    feat: Create bubble_sort method (empty)

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,7 @@
 # For every element in the array, look at the element and the element directly to the right of it
 # If out of order, swap the two elements
 # Repeat a maximum of n-1 times, where n is the number of elements in the array
+
+def bubble_sort(list)
+  
+end
