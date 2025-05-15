8b85ffbc3aa1cfb6c6726228dc7f1984cd3120a4
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-15 10:50:06 -0700

    feat: Sort array a max of n-1 times
    Include optimization where algorithm will break after detecting 1 or
    0 swaps in list/array order

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,13 +3,24 @@
 # Repeat a maximum of n-1 times, where n is the number of elements in the array
 
 def bubble_sort(list)
-  list.each_with_index do |left_val, left|
-    right = left + 1
-    right_val = list[right]
-    next if right_val.nil?
-    if left_val > right_val
-      list[left], list[right] = list[right], list[left]
+  iterations = list.length - 1
+  sorted = false
+  
+  iterations.downto(1) do |i|
+    break unless sorted == false
+    swaps = 0
+
+    list.each_with_index do |left_val, left|
+      right = left + 1
+      right_val = list[right]
+      next if right_val.nil?
+      if left_val > right_val
+        list[left], list[right] = list[right], list[left]
+        swaps += 1
+      end
     end
+    
+    sorted = (swaps < 2) ? true : false
   end
 
   list
