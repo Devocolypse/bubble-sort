54c640eee3ac9a98cc05cb7b72f67480da7efc47
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-15 09:37:57 -0700

    docs(src): Add basic pseudocode

---
+++ bubble_sort.rb
@@ -0,0 +1,3 @@
+# For every element in the array, look at the element and the element directly to the right of it
+# If out of order, swap the two elements
+# Repeat a maximum of n-1 times, where n is the number of elements in the array
