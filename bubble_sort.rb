74689bf3da15bbc2e1ad22c9edb0bc827a52dc14
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-15 09:43:34 -0700

    feat: Iterate over list and echo element/position

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,5 +3,9 @@
 # Repeat a maximum of n-1 times, where n is the number of elements in the array
 
 def bubble_sort(list)
-  
+  list.each_with_index do |elm, pos|
+    puts "Element #{elm} is located at position: #{pos}"
+  end
 end
+
+bubble_sort([4,3,78,2,0,2])
