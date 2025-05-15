f674b813f7eb99f43d3554d0faa02da6c2693674
Devocolypse <72180176+Devocolypse@users.noreply.github.com>
2025-05-15 10:23:25 -0700

    feat: Do one pass of sorting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,9 +3,16 @@
 # Repeat a maximum of n-1 times, where n is the number of elements in the array
 
 def bubble_sort(list)
-  list.each_with_index do |elm, pos|
-    puts "Element #{elm} is located at position: #{pos}"
+  list.each_with_index do |left_val, left|
+    right = left + 1
+    right_val = list[right]
+    next if right_val.nil?
+    if left_val > right_val
+      list[left], list[right] = list[right], list[left]
+    end
   end
+
+  list
 end
 
-bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
